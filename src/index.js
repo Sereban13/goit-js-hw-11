9258034a9a9cf5552b27bdaed8b74a82c96045ad
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-05-07 17:28:05 +0200

    task-update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,77 @@
-// import axios from 'axios';
-// import Notiflix, { Loading } from 'notiflix';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const form = document.querySelector('#search-form');
-const input = document.querySelector('[name="searchQuery"]');
 const photoResults = document.querySelector('.gallery');
+const inputQuery = document.querySelector('[name="searchQuery"]');
+const loadBtn = document.querySelector('.load-more');
 
-const textRequest = input.value;
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '36106803-d64240cda904fbe2d47e1c8e1';
 
-function getPhoto(request) {
-  return fetch(
-    `//pixabay.com/api/?key=36106803-d64240cda904fbe2d47e1c8e1&q="${request}"&image_type="photo"&orientantion="horizontal"&safesearch=true`
-  ).then(responce => {
-    if (!responce.ok) {
-      throw new Error(responce.statusText);
-    }
-    return responce.json();
-  });
-}
-
-form.addEventListener('submit', onSubmit);
+let currentPage = 11;
+let searchQuery = '';
 
 function onSubmit(event) {
   event.preventDefault();
-  testRequest = input.value;
+  searchQuery = inputQuery.value;
 
-  getPhoto(testRequest)
-    .then(data => {
-      photoResults.innerHTML = galleryCraete(data.hits);
+  getPhoto(searchQuery, currentPage)
+    .then(responce => {
+      console.log(responce);
+      const newData = responce.data;
+      if (newData.hits.length === 0) {
+        loadBtn.hidden = true;
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else if (currentPage >= newData.totalHits / 40) {
+        loadBtn.hidden = true;
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        photoResults.innerHTML = galleryCraete(newData.hits);
+        loadBtn.hidden = false;
+        Notiflix.Notify.success(
+          `Hooray! We found ${newData.totalHits} images.`
+        );
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(`Sorry, something goes wrong`);
+    });
+}
+
+function loadNewPagination() {
+  currentPage += 1;
+  searchQuery = inputQuery.value;
+
+  getPhoto(searchQuery, currentPage)
+    .then(responce => {
+      const newData = responce.data;
+      photoResults.insertAdjacentHTML('beforeend', galleryCraete(newData.hits));
+      if (currentPage >= newData.totalHits / 40) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadBtn.hidden = true;
+      }
     })
     .catch(error => console.log(error));
+}
+
+async function getPhoto(searchQuery, currentPage = 1) {
+  try {
+    const responce = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${currentPage}&image_type="photo"&orientantion="horizontal"&safesearch=true&per_page=40`
+    );
+    return responce;
+  } catch (error) {
+    Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
 }
 
 function galleryCraete(photoGallery) {
@@ -55,3 +98,5 @@ function galleryCraete(photoGallery) {
     )
     .join('');
 }
+loadBtn.addEventListener('click', loadNewPagination);
+form.addEventListener('submit', onSubmit);
